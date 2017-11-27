60b2d256ae37a551da97598f43cac98e37357fdd
Gowthaman Gobalasingam <gowthaman.gobalasingam@stud.unibas.ch>
2017-11-27 11:53:45 +0100

    Deklared first struct and some important methods

--- LEDCube_SNAKE/LEDCube_SNAKE.ino
+++ LEDCube_SNAKE/LEDCube_SNAKE.ino
@@ -1,3 +1,28 @@
+/* 3D Snake game for the lecture computer architecture and operating systems.
+We gonna build a 8x8x8 Led-Cube, then write a snake program, which we can play with a joystick
+*/
+
+struct Coordinate{
+    int x;
+    int y;
+    int z;
+    int direction;
+}
+
+struct Snake{
+    struct Coordinate snakeList;
+    int SnakeListLength;
+}
+
+moveUp();
+moveDown();
+moveLeft();
+moveRight();
+startGame();
+eatApple();
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
